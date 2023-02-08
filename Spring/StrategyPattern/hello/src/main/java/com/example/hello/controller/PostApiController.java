7f57985aea80daac4372d59e1bc4e28ca583093b
username <ieejo716@naver.com>
2023-02-08 21:58:28 +0900

    POST : 객체를 활용한 post

--- Spring/StrategyPattern/hello/src/main/java/com/example/hello/controller/PostApiController.java
+++ Spring/StrategyPattern/hello/src/main/java/com/example/hello/controller/PostApiController.java
@@ -1,5 +1,6 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserInfoReq;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,12 @@ public class PostApiController {
         requestData.forEach((key, value) ->
                 System.out.println("key : " + key + " , value : " + value)
         );
+    }
+
+    //GET Mapping 같은 경우는 객체를 사용할 때 어노테이션을 붙이지 않아도 상관없었지만
+    //POST 는 @RequestBody를 사용해야지만 값을 받아올 수 있습니다.
+    @PostMapping("/user-info")
+    public UserInfoReq postUserInfo(@RequestBody UserInfoReq userInfoData) {
+        return userInfoData;
     }
 }
